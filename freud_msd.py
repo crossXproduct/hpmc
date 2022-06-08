cff912e2654656c90d7da01b0b903eaa85340796
crossXproduct <elise.aaron@gmail.com>
2022-06-07 23:28:59 -0400

    complete draft of freud_msd

---
+++ freud_msd.py
@@ -0,0 +1,21 @@
+import freud
+import gsd.hoomd
+import scipy
+import numpy as np
+
+path = str(sys.argv[1])
+traj = gsd.hoomd.open(path,'rb')
+
+print('frames: ',len(traj))
+print('particles: ',len(traj[0].particles.position))
+print('first snapshot: ',traj[0].particles.position[0:10])
+
+print(traj[0].box,traj[1].box)
+msd = freud.msd.MSD(traj[0].box)
+
+msd.compute(traj[:].particles.position[:])
+
+print('msd[0:10]',msd.msd[0:11])
+print('msd[0:10]',msd.msd[344388:344399])
+
+traj.close()
