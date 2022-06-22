2fcab784883277295e9b895a6c7788a8745fb81a
crossXproduct <elise.aaron@gmail.com>
2022-06-22 07:09:00 -0400

    simplified init script

--- init.sh
+++ init.sh
@@ -6,14 +6,12 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 #IMPORTANT: this template is meant to be used only by the continue.sh "driver" script for submitting continuous runs.
-read;
 cp $SLURM_SUBMIT_DIR/init.in $TMPDIR
-"$REPLY" >> init.in
 cd /fs/project/PHS0243/crossproduct/simulations/
 cp hoomd_init.py $TMPDIR
 cd $TMPDIR
 module load miniconda3
 source activate sims
-python3 hoomd_init.py $REPLY
+python3 hoomd_init.py < init.in
 cp restart.gsd $SLURM_SUBMIT_DIR
 cp compressed.gsd $SLURM_SUBMIT_DIR
