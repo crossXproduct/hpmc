94bbbabaadde27ac3b5e8a0d50ea9f0929fd4da0
crossXproduct <elise.aaron@gmail.com>
2022-06-22 05:10:57 -0400

    bash fixes

--- continue.sh
+++ continue.sh
@@ -4,20 +4,20 @@
 echo "Initializing System..."
 CONTINUE="False"
 CONTINUE=$(sbatch init.sh < CONTINUE)
-while[$CONTINUE -eq "True"]
+while[$CONTINUE -eq "True"];
 do CONTINUE=$(sbatch init.sh < CONTINUE)
 done
 #equilibrate
 echo "Equilibrating System..."
 CONTINUE=$(sbatch equil.sh < CONTINUE)
-while[$CONTINUE -eq "True"]
+while[$CONTINUE -eq "True"];
 do
 CONTINUE=$(sbatch equil.sh < CONTINUE)
 done
 #run
 echo "Running Simulation..."
 CONTINUE=$(sbatch equil.sh < CONTINUE)
-while[$CONTINUE -eq "True"]
+while[$CONTINUE -eq "True"];
 do
 CONTINUE=$(sbatch equil.sh < CONTINUE)
 done
