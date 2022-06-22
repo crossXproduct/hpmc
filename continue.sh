8af66dac9c84d9cce75b247fe4ef5baa9b95de7a
crossXproduct <elise.aaron@gmail.com>
2022-06-22 05:46:22 -0400

    bash fixes

--- continue.sh
+++ continue.sh
@@ -3,27 +3,27 @@
 #initialize
 echo "Initializing System..."
 CONTINUE=False
-RESTART=$(sbatch init.sh CONTINUE | awk 'NR==1{print $5}')
+RESTART=$(sbatch init.sh CONTINUE | awk 'NR==2{print $1}')
 while[$RESTART -eq "True"];
 do
-CONTINUE=$(sbatch init.sh RESTART | awk 'NR==1{print $5}')
+CONTINUE=$(sbatch init.sh RESTART | awk 'NR==2{print $1}')
 RESTART=CONTINUE
 done
 #equilibrate
 echo "Equilibrating System..."
 CONTINUE=False
-RESTART=$(sbatch equil.sh CONTINUE | awk 'NR==1{print $5}')
+RESTART=$(sbatch equil.sh CONTINUE | awk 'NR==2{print $1}')
 while[$RESTART -eq "True"];
 do
-CONTINUE=$(sbatch equil.sh RESTART | awk 'NR==1{print $5}')
+CONTINUE=$(sbatch equil.sh RESTART | awk 'NR==2{print $1}')
 RESTART=CONTINUE
 done
 #run
 echo "Running Simulation..."
 CONTINUE=False
-RESTART=$(sbatch equil.sh CONTINUE | awk 'NR==1{print $5}')
+RESTART=$(sbatch equil.sh CONTINUE | awk 'NR==2{print $1}')
 while[$RESTART -eq "True"];
 do
-CONTINUE=$(sbatch equil.sh RESTART | awk 'NR==1{print $5}')
+CONTINUE=$(sbatch equil.sh RESTART | awk 'NR==2{print $1}')
 RESTART=CONTINUE
 done
