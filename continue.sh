ecdaff1ff072aede41979428d6d576fc66308e85
crossXproduct <elise.aaron@gmail.com>
2022-06-22 05:24:12 -0400

    bash fixes

--- continue.sh
+++ continue.sh
@@ -3,27 +3,27 @@
 #initialize
 echo "Initializing System..."
 CONTINUE=False
-RESTART=$(sbatch init.sh < CONTINUE)
+RESTART=$(sbatch init.sh CONTINUE)
 while[$RESTART -eq "True"];
 do
-CONTINUE=$(sbatch init.sh < RESTART)
+CONTINUE=$(sbatch init.sh RESTART)
 RESTART=CONTINUE
 done
 #equilibrate
 echo "Equilibrating System..."
 CONTINUE=False
-RESTART=$(sbatch equil.sh < CONTINUE)
+RESTART=$(sbatch equil.sh CONTINUE)
 while[$RESTART -eq "True"];
 do
-CONTINUE=$(sbatch equil.sh < RESTART)
+CONTINUE=$(sbatch equil.sh RESTART)
 RESTART=CONTINUE
 done
 #run
 echo "Running Simulation..."
 CONTINUE=False
-RESTART=$(sbatch equil.sh < CONTINUE)
+RESTART=$(sbatch equil.sh CONTINUE)
 while[$RESTART -eq "True"];
 do
-CONTINUE=$(sbatch equil.sh < RESTART)
+CONTINUE=$(sbatch equil.sh RESTART)
 RESTART=CONTINUE
 done
